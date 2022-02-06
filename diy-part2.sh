497c6e1250ad5e9caa48f520156e205d0009723a
LondyGhost <476729421@qq.com>
2022-02-06 15:09:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-# rm -rf target/linux/ipq806x/files/arch/arm/boot/dts/qcom-ipq8064-d7800.dts
-# mkdir dts
-# git clone https://github.com/LondyGhost/DL.git dts
-# cp -rf dts/qcom-ipq8064-d7800.dts target/linux/ipq806x/files/arch/arm/boot/dts/
+sed -i '82{s/4:wan/4:lan/}' /target/linux/ramips/mt7621/base-files/etc/board.d/02_network
