8d0208ff613cdc7dbf62d323cb12c849a6bb44a4
LondyGhost <476729421@qq.com>
2022-02-22 16:28:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-cd lede/package/lean
-git clone https://github.com/thinktip/luci-theme-neobird.git
-cd ~/lede/
+git clone https://github.com/thinktip/luci-theme-neobird.git package/lean
