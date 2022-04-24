692125f4509bee86ac117c8ff1fded482ea00bde
LondyGhost <476729421@qq.com>
2022-04-24 22:57:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i '82{s/4:wan/4:lan/}' /target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-cp -i copy/makefile /feeds/packages/net/transmission
+#sed -i '82{s/4:wan/4:lan/}' /target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+cp -i /copy/makefile /feeds/packages/net/transmission
